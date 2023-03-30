b89d10e13885b8f47dceb878408f2ce7008470e8
ryan-dfw <mckevitt.r@gmail.com>
2023-03-30 15:47:33 -0500

    refactored to use mpsc instead of Arc Mutex

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use pipeviewer::{args::Args, read, stats, write};
 use std::io::Result;
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
 use std::thread;
 
 fn main() -> Result<()> {
     let args = Args::parse();
     let Args {
-        infiles,
+        infile,
         outfile,
         silent,
     } = args;
 
-    let quit = Arc::new(Mutex::new(false));
-    let (quit1, quit2, quit3) = (quit.clone(), quit.clone(), quit.clone());
+    let (stats_tx, stats_rx) = mpsc::channel();
+    let (write_tx, write_rx) = mpsc::channel();
 
-    let read_handle = thread::spawn(move || read::read_loop(&infile, quit1));
-    let stats_handle = thread::spawn(move || stats::stats_loop(silent, quit2));
-    let write_handle = thread::spawn(move || write::write_loop(&outfile, quit3));
+    let read_handle = thread::spawn(move || read::read_loop(&infile, stats_tx));
+    let stats_handle = thread::spawn(move || stats::stats_loop(silent, stats_rx, write_tx));
+    let write_handle = thread::spawn(move || write::write_loop(&outfile, write_rx));
 
     // crash if any threads have crashed
     // '.join()' returns a 'thread::Result<io::Result<()>>'
@@ -29,14 +29,6 @@ fn main() -> Result<()> {
     read_io_result?;
     stats_io_result?;
     write_io_result?;
-
-
-        stats::stats(args.silent, buffer.len(), &mut total_bytes, false);
-        if write::write(&args.outfile, &buffer)? {
-            break;
-        }
-    }
-    stats::stats(args.silent, 0, &mut total_bytes, true);
 
     Ok(())
 }
